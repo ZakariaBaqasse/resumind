1ad1d69da9e855d0218f467d6fef84024f84a954
Zakaria Baqasse <zbaqasse51@gmail.com>
2025-09-27 16:23:45 +0100

    fix: update type definition for children prop in ProtectedComponentsLayout

--- apps/web/app/(protected-components)/layout.tsx
+++ apps/web/app/(protected-components)/layout.tsx
@@ -1,12 +1,10 @@
 "use client"
 
-import { ReactNode } from "react"
-
 import ProtectedComponents from "@/components/auth/protected-component"
 
 export default function ProtectedComponentsLayout({
   children,
-}: Readonly<{ children: ReactNode }>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <ProtectedComponents>
       <div className="min-h-screen bg-gray-50">{children}</div>
